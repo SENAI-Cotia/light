19c6cafad3c20c6b77381573fac3da9ad9cf21bd
Nicole Carvalho <91771078+NicoleCarvalh@users.noreply.github.com>
2026-05-13 16:54:11 -0300

    feat: Continuação do desenvolvimento da dashboard

--- src/main/resources/static/js/actions.js
+++ src/main/resources/static/js/actions.js
@@ -11,7 +11,6 @@ toggle.addEventListener('click', () => {
     const isOpened = toggle.classList.toggle('open')
     nav.classList.toggle('menu-open', isOpened)
 
-
     if (isOpened) {
         path1.setAttribute('d', 'M7 7 L25 25');
         path3.setAttribute('d', 'M7 25 L25 7');
