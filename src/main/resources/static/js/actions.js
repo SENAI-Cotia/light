85dd447686397409a2b754bdd22c598817a68e52
Nicole Carvalho <91771078+NicoleCarvalh@users.noreply.github.com>
2026-05-18 10:28:22 -0300

    fix: Correção de bug no .js para página de login

--- src/main/resources/static/js/actions.js
+++ src/main/resources/static/js/actions.js
@@ -6,16 +6,17 @@ const path1  = document.getElementById('path1');
 const path2  = document.getElementById('path2');
 const path3  = document.getElementById('path3');
 
+if(toggle) {
+    toggle.addEventListener('click', () => {
+        const isOpened = toggle.classList.toggle('open')
+        nav.classList.toggle('menu-open', isOpened)
 
-toggle.addEventListener('click', () => {
-    const isOpened = toggle.classList.toggle('open')
-    nav.classList.toggle('menu-open', isOpened)
-
-    if (isOpened) {
-        path1.setAttribute('d', 'M7 7 L25 25');
-        path3.setAttribute('d', 'M7 25 L25 7');
-    } else {
-        path1.setAttribute('d', 'M4 7h24');
-        path3.setAttribute('d', 'M4 25h24');
-    }
-})
+        if (isOpened) {
+            path1.setAttribute('d', 'M7 7 L25 25');
+            path3.setAttribute('d', 'M7 25 L25 7');
+        } else {
+            path1.setAttribute('d', 'M4 7h24');
+            path3.setAttribute('d', 'M4 25h24');
+        }
+    })
+}
