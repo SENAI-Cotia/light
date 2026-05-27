8b27cc8a2d97125b743975ae14559cfd47b4f6ee
Nicole Carvalho <91771078+NicoleCarvalh@users.noreply.github.com>
2026-05-27 10:55:25 -0300

    feat: Continuação design da página de dashboard

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -1,16 +1,18 @@
 Chart.defaults.font.family = "'Nunito Sans', sans-serif";
 
+const anoAtual = new Date().getFullYear();
+
 new Chart(document.getElementById('kofiChart'), {
     type: 'line',
     data: {
         labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul','Ago', 'Set', 'Out', 'Nov', 'Dez'],
         datasets: [{
             label: 'Vendas mensais',
-            data: [12, 19, 3, 5, 2, 70, 8, 5, 0, 56, 25, 60],
+            data: [95, 102, 132, 153, 185, 10, 0, 0, 0, 0, 0, 0],
             borderColor: '#41210A',
             borderWidth: 3,
             pointRadius: 1,
-            pointHoverRadius: 0,
+            pointHoverRadius: 5,
             tension: 0.4,
             fill: true,
             backgroundColor: '#F1EADA'
@@ -32,7 +34,7 @@ new Chart(document.getElementById('kofiChart'), {
             },
             title: {
                 display: true,
-                text: 'Quantidade de pedidos',
+                text: 'Quantidade de pedidos - Ano: ' + anoAtual,
                 color: '#3b2a1a',
                 font: {
                     size: 16,
